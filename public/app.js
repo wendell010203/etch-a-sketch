111d53808a446352e23f6c4b5af2f3be95958013
Wendell Hernandez <wendell010203@gmail.com>
2023-11-23 20:56:59 +0800

    Done with simple 16x16 square

--- public/app.js
+++ public/app.js
@@ -1,7 +1,23 @@
 window.addEventListener('load' , () => {
 
-console.log("loaded");
+let grid = 16;
 
+const squareContainer = document.querySelector('.squareContainer');
 
+for(let i = 1; i <= grid*grid; i++){
+    let square = document.createElement('div');
+    square.classList.toggle('square');
 
+    squareContainer.appendChild(square);
+}
+
+const square = document.querySelectorAll('.square');
+
+square[1].addEventListener('mouseover' , () => {
+    console.log("object");
+});
+
+squareContainer.addEventListener('mouseover' , (e) => {
+    e.target.style = "background-color: #333";
+});
 });
